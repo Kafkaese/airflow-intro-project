a4409c50300e81c408365fc9de87acba0ed8c542
Frederik Laubisch <kafkaese@gmail.com>
2022-10-28 12:48:05 +0200

    move from subdags to groups

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,9 +1,8 @@
 from operator import sub
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.subdag import SubDagOperator
-from subdags.subdag_download import subdag_downlaod
-from subdags.subdag_transform import subdag_transform
+from groups.group_download import downlaod_tasks
+from groups.group_transform import trandform_tasks
 from datetime import datetime
     
 with DAG('group_dag', start_date=datetime(2022, 1, 1), 
@@ -11,19 +10,14 @@ with DAG('group_dag', start_date=datetime(2022, 1, 1),
     
     args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
     
-    downloads = SubDagOperator(
-        task_id = 'downloads',
-        subdag=subdag_downlaod(dag.dag_id, 'downloads', args))
+    downloads = downlaod_tasks()
     
     check_files = BashOperator(
         task_id='check_files',
         bash_command='sleep 10'
     )
     
-    transforms = SubDagOperator(
-        task_id = 'tranforms',
-        subdag=subdag_transform(dag.dag_id, 'tranforms', args))
-    
+    transforms = trandform_tasks()
     
     downloads >> check_files >> transforms
 
