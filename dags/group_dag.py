4f398b306aa84fb27432151cc86e89899ec41fd1
Frederik Laubisch <kafkaese@gmail.com>
2022-10-28 12:09:29 +0200

    use subdags download and tranform in group dag

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,45 +1,29 @@
-    from airflow import DAG
-    from airflow.operators.bash import BashOperator
-     
-    from datetime import datetime
-     
-    with DAG('group_dag', start_date=datetime(2022, 1, 1), 
-        schedule_interval='@daily', catchup=False) as dag:
-     
-        download_a = BashOperator(
-            task_id='download_a',
-            bash_command='sleep 10'
-        )
-     
-        download_b = BashOperator(
-            task_id='download_b',
-            bash_command='sleep 10'
-        )
-     
-        download_c = BashOperator(
-            task_id='download_c',
-            bash_command='sleep 10'
-        )
-     
-        check_files = BashOperator(
-            task_id='check_files',
-            bash_command='sleep 10'
-        )
-     
-        transform_a = BashOperator(
-            task_id='transform_a',
-            bash_command='sleep 10'
-        )
-     
-        transform_b = BashOperator(
-            task_id='transform_b',
-            bash_command='sleep 10'
-        )
-     
-        transform_c = BashOperator(
-            task_id='transform_c',
-            bash_command='sleep 10'
-        )
-     
-        [download_a, download_b, download_c] >> check_files >> [transform_a, transform_b, transform_c]
+from operator import sub
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from subdags.subdag_download import subdag_downlaod
+from subdags.subdag_transform import subdag_transform
+from datetime import datetime
+    
+with DAG('group_dag', start_date=datetime(2022, 1, 1), 
+    schedule_interval='@daily', catchup=False) as dag:
+    
+    args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
+    
+    downloads = SubDagOperator(
+        task_id = 'downloads',
+        subdag=subdag_downlaod(dag.dag_id, 'downloads', args))
+    
+    check_files = BashOperator(
+        task_id='check_files',
+        bash_command='sleep 10'
+    )
+    
+    transforms = SubDagOperator(
+        task_id = 'tranforms',
+        subdag=subdag_transform(dag.dag_id, 'tranforms', args))
+    
+    
+    downloads >> check_files >> transforms
 
